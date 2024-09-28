600ef5751c22afb38d21a048394c7bf873adaebc
João Vítor F Martiniano <115907286+JoaoV-Mart@users.noreply.github.com>
2024-09-28 16:47:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioProjetoSmartphone.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("4987", "Modelo2", "22222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
